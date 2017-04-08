9cbb46cf3c1178b1dd1b431d32ca851987eccd6a
Mandla <mandla.magagula@gmail.com>
2017-04-08 13:59:10 +0200

    Auth 0 View added

--- src/modules/AppView.js
+++ src/modules/AppView.js
@@ -1,19 +1,18 @@
-import React, {PropTypes, Component} from 'react';
-import {View, StyleSheet, StatusBar, ActivityIndicator} from 'react-native';
-import NavigatorViewContainer from './navigator/NavigatorViewContainer';
+import React, {PropTypes} from 'react';
+import {View, StyleSheet, ActivityIndicator} from 'react-native';
+import NavigationViewContainer from './navigator/NavigatorViewContainer';
+import * as auth0 from '../services/auth0';
 import * as snapshotUtil from '../utils/snapshot';
 import * as SessionStateActions from '../modules/session/SessionState';
 import store from '../redux/store';
 import DeveloperMenu from '../components/DeveloperMenu';
 
-class AppView extends Component {
-  static displayName = 'AppView';
-
-  static propTypes = {
+const AppView = React.createClass({
+  propTypes: {
     isReady: PropTypes.bool.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
     dispatch: PropTypes.func.isRequired
-  };
-
+  },
   componentDidMount() {
     snapshotUtil.resetSnapshot()
       .then(snapshot => {
@@ -29,26 +28,33 @@ class AppView extends Component {
           snapshotUtil.saveSnapshot(store.getState());
         });
       });
-  }
+  },
+
+  componentWillReceiveProps({isReady, isLoggedIn}) {
+    if (!this.props.isReady) {
+      if (isReady && !isLoggedIn) {
+        auth0.showLogin();
+      }
+    }
+  },
 
   render() {
     if (!this.props.isReady) {
       return (
-        <View style={{flex: 1}}>
-          <ActivityIndicator style={styles.centered} />
+        <View>
+          <ActivityIndicator style={styles.centered}/>
         </View>
       );
     }
 
     return (
       <View style={{flex: 1}}>
-        <StatusBar backgroundColor='#455a64' barStyle='light-content' />
-        <NavigatorViewContainer />
+        <NavigationViewContainer />
         {__DEV__ && <DeveloperMenu />}
       </View>
     );
   }
-}
+});
 
 const styles = StyleSheet.create({
   centered: {
