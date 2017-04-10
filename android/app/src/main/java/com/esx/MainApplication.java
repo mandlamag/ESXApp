e33ca7a4736d196e8e2a1896960befa817f7090b
Mandla <mandla.magagula@gmail.com>
2017-04-10 04:09:18 +0200

    Add mobile center and crash analytics

--- android/app/src/main/java/com/esx/MainApplication.java
+++ android/app/src/main/java/com/esx/MainApplication.java
@@ -3,6 +3,8 @@ package com.esx;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.azure.mobile.react.crashes.RNCrashesPackage;
+import com.microsoft.azure.mobile.react.analytics.RNAnalyticsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.auth0.lock.react.LockReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
+            new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics)),
             new VectorIconsPackage(),
             new LockReactPackage()
       );
--- ios/Pods/Target Support Files/Pods-esx/Pods-esx-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_esx : NSObject
-@end
-@implementation PodsDummy_Pods_esx
-@end
