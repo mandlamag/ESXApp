d30a4dad981b56a6d7cffb89f6f34466d7767c79
Mandla <mandla.magagula@gmail.com>
2017-04-11 07:05:55 +0200

    Add vector icons

--- android/app/src/main/java/com/esx/MainApplication.java
+++ android/app/src/main/java/com/esx/MainApplication.java
@@ -3,10 +3,10 @@ package com.esx;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.microsoft.azure.mobile.react.crashes.RNCrashesPackage;
-import com.microsoft.azure.mobile.react.analytics.RNAnalyticsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.auth0.lock.react.LockReactPackage;
+import com.microsoft.azure.mobile.react.crashes.RNCrashesPackage;
+import com.microsoft.azure.mobile.react.analytics.RNAnalyticsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,10 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
-            new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics)),
             new VectorIconsPackage(),
-            new LockReactPackage()
+            new LockReactPackage(),
+            new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
+            new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics))
       );
     }
   };
